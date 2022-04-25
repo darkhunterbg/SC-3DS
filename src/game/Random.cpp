88ebf47ebd19425585a030b0c631b72127dacc24
Kaloyan Nachev <kaloyan.r.nachev@gmail.com>
2022-04-25 23:26:22 +0300

    Random fix kinda

--- src/game/Random.cpp
+++ src/game/Random.cpp
@@ -16,5 +16,13 @@ Random::Random(uint32_t seed):
 	seed(seed)
 {
 }
+int Random::Next()
+{
+	// https://stackoverflow.com/questions/26237419/faster-than-rand
+	int i = seed;
+	seed = (214013 * seed + 2531011);
+	seed = (seed >> 16) & 0x7FFF;
+	return i;
+}
 
 
